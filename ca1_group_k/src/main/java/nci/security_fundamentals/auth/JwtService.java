ab458015a918346054960a6677db9538f3b64fdf
Andre <andrepontde@gmail.com>
2025-11-04 12:58:43 +0000

    Added extra changes for mongoDB deployment

--- ca1_group_k/src/main/java/nci/security_fundamentals/auth/JwtService.java
+++ ca1_group_k/src/main/java/nci/security_fundamentals/auth/JwtService.java
@@ -9,30 +9,30 @@ import com.auth0.jwt.interfaces.DecodedJWT;
 
 public class JwtService {
 
-    public String getToken(String data) {
-        try {
-            Algorithm algorithm = Algorithm.RSA256(rsaPublicKey, rsaPrivateKey);
-            String token = JWT.create()
-                    .withIssuer("auth0")
-                    .sign(algorithm);
-        } catch (JWTCreationException exception) {
-            // Invalid Signing configuration / Couldn't convert Claims.
-        }
-    }
+    // public String getToken(String data) {
+    //     try {
+    //         Algorithm algorithm = Algorithm.RSA256(rsaPublicKey, rsaPrivateKey);
+    //         String token = JWT.create()
+    //                 .withIssuer("auth0")
+    //                 .sign(algorithm);
+    //     } catch (JWTCreationException exception) {
+    //         // Invalid Signing configuration / Couldn't convert Claims.
+    //     }
+    // }
 
-    public String validateToken(String token) {
-        DecodedJWT decodedJWT;
-        try {
-            Algorithm algorithm = Algorithm.RSA256(rsaPublicKey, rsaPrivateKey);
-            JWTVerifier verifier = JWT.require(algorithm)
-                    // specify any specific claim validations
-                    .withIssuer("auth0")
-                    // reusable verifier instance
-                    .build();
+    // public String validateToken(String token) {
+    //     DecodedJWT decodedJWT;
+    //     try {
+    //         Algorithm algorithm = Algorithm.RSA256(rsaPublicKey, rsaPrivateKey);
+    //         JWTVerifier verifier = JWT.require(algorithm)
+    //                 // specify any specific claim validations
+    //                 .withIssuer("auth0")
+    //                 // reusable verifier instance
+    //                 .build();
 
-            decodedJWT = verifier.verify(token);
-        } catch (JWTVerificationException exception) {
-            // Invalid signature/claims
-        }
-    }
+    //         decodedJWT = verifier.verify(token);
+    //     } catch (JWTVerificationException exception) {
+    //         // Invalid signature/claims
+    //     }
+    // }
 }
